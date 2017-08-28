38eea3f57bbd3c7f6534ff19ddda0ee8adc053fb
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2017-08-28 00:50:52 -0700

    Now nyc will use the non-transpiled version

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
 /* eslint-disable flowtype/require-parameter-type, flowtype/require-return-type, no-magic-numbers */
 import {test} from "tap"
 
-import pipe from "./"
+import pipe from "./index"
 
 const increment = (value) => value + 1
 const toString = (value) => `${value}`
