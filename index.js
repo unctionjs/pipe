40700add140d9efa3fa1f03bb87eb11ca6ce220f
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-04-21 15:52:53 -0700

    Lint

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import thrush from "@unction/thrush";
 import reduceValues from "@unction/reducevalues";
+
 export default function pipe (unctions) {
   return function pipeUnctions (initial) {
     return reduceValues(thrush)(initial)(unctions);
