a2adf6fe8101e1d70b2513219614cd610e9aded5
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-04-21 15:53:08 -0700

    Adding an extra authoritative test

--- README.md
+++ README.md
@@ -10,6 +10,7 @@ Takes a list of functions and runs a value through that stack from left to right
 
 ``` javascript
 pipe([toString, toInteger])(0) // 0
+pipe([append("b"), append("a")])("c") // "cba"
 ```
 
 [BADGE_TRAVIS]: https://img.shields.io/travis/unctionjs/pipe.svg?maxAge=2592000&style=flat-square
--- test.js
+++ test.js
@@ -6,6 +6,7 @@ import pipe from "./index";
 const increment = (value) => value + 1;
 const toString = (value) => `${value}`;
 const toInteger = (value) => parseInt(value, 10);
+const append = (left) => (right) => `${right}${left}`;
 
 test(({same, end}) => {
   same(
@@ -20,6 +21,15 @@ test(({same, end}) => {
   same(
     pipe([toString, toInteger])(0),
     0
+  );
+
+  end();
+});
+
+test(({equal, end}) => {
+  equal(
+    pipe([append("b"), append("a")])("c"),
+    "cba"
   );
 
   end();
