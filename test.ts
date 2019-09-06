f8040c3140c22f95be39e8262cd9d7e7aa0ece1b
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-05 22:58:51 -0700

    Moving to jest and typing the test file

---
+++ test.ts
@@ -0,0 +1,19 @@
+/* eslint-disable no-magic-numbers */
+import pipe from "./index.ts";
+
+const increment = (value) => value + 1;
+const toString = (value) => `${value}`;
+const toInteger = (value) => parseInt(value, 10);
+const append = (left) => (right) => `${right}${left}`;
+
+test(() => {
+  expect(pipe([increment, increment, increment])(0)).toEqual(3);
+});
+
+test(() => {
+  expect(pipe([toString, toInteger])(0)).toEqual(0);
+});
+
+test(() => {
+  expect(pipe([append("b"), append("a")])("c")).toBe("cba");
+});
