a498e1e5435ca1ef777c477914fedbdf90fabd14
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-09-06 13:08:10 -0700

    Continuing to setup testing

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-magic-numbers */
-import pipe from "./index.ts";
+import pipe from "./index";
 
 const increment = (value) => value + 1;
 const toString = (value) => `${value}`;
